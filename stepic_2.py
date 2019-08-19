78861766f70496b7b9f0e046daeccc8bd9877588
Ditriy Anishchenko <1348010@gmail.com>
2019-08-19 11:35:55 +0300

    Решение задачи Палиндром при помощи среза

--- stepic_2.py
+++ stepic_2.py
@@ -346,17 +346,25 @@ import time
 """
 Проверить, является ли строка полиндромом
 """
+# # Вариант 1
+# s = input()
+# i = 0
+# j = len(s) - 1
+# is_palindrom = True
+# while i < j:
+#     if s[i]!=s[j]:
+#         is_palindrom = False
+#     i += 1
+#     j -= 1
+# if is_palindrom:
+#     print("Yes")
+# else:
+#     print("No")
 
+# Вариант 2
 s = input()
-i = 0
-j = len(s) - 1
-is_palindrom = True
-while i < j:
-    if s[i]!=s[j]:
-        is_palindrom = False
-    i += 1
-    j -= 1
-if is_palindrom:
-    print("Yes")
+r = s[::-1]
+if s == r:
+    print('YES')
 else:
-    print("No")
+    print('NO')
